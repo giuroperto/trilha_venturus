976ea04b9b4fc0eef392e8ab82b280585ce56c77
giuroperto <giu.roperto@gmail.com>
2021-08-12 11:00:44 -0300

    unsuccessful attempt to change asynctask to thread without a handler

--- TrilhaVenturus_Ex5/app/src/main/java/com/gr/android/trilhaventurus_ex5/MainActivity.kt
+++ TrilhaVenturus_Ex5/app/src/main/java/com/gr/android/trilhaventurus_ex5/MainActivity.kt
@@ -35,17 +35,11 @@ class MainActivity : AppCompatActivity() {
         btnDownload.setOnClickListener {
             Log.i("AsyncTask", "CLICKLISTENER - Button was clicked. Thread: " +
                 Thread.currentThread().name)
-            callAsyncTask(edtUrl.text.toString())
+
+            callNewThread(edtUrl.text.toString())
+
+//            callAsyncTask(edtUrl.text.toString())
         }
-    }
-
-    private fun callAsyncTask(url: String) {
-        val download: DownloadTask = DownloadTask()
-
-        Log.i("AsyncTask", "CALLING TASK - Async being called. Thread: " +
-            Thread.currentThread().name)
-
-        download.execute(url)
     }
 
     private fun setImage(image: Bitmap?) {
@@ -58,6 +52,75 @@ class MainActivity : AppCompatActivity() {
         message?.let {
             Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
         }
+    }
+
+    private fun callNewThread(url: String) {
+        Log.i("AsyncTask", "CALLING NEW THREAD - New Thread being called. Thread: " +
+                Thread.currentThread().name)
+
+        Thread {
+            Log.i("AsyncTask", "INSIDE NEW THREAD - New thread. Thread: " +
+                    Thread.currentThread().name)
+//            showMessage("Please wait! Downloading image...")
+            doInBackgroundStyle(url)
+        }.start()
+    }
+
+    private fun doInBackgroundStyle(url: String) {
+        var imageBitmap: Bitmap? = null
+        var progress: Int = 0
+
+        try {
+            Log.i("AsyncTask", "DOINBACK TRY - Downloading image. Thread: " +
+                    Thread.currentThread().name)
+            imageBitmap = downloadImage(url)
+            for (i in 0..2) {
+                progress = (i * 100)/2
+//                showMessage("${progress.toString()}%")
+            }
+        } catch (e: IOException) {
+            Log.i("AsyncTask", "DOINBACK CATCH - " + e.message)
+        }
+
+        onPostExecuteStyle(imageBitmap)
+    }
+
+    @Throws(IOException::class)
+    private fun downloadImage(url: String?) : Bitmap {
+        val urlAddress : URL
+        val inputStream : InputStream
+        val image : Bitmap
+
+        urlAddress = URL(url)
+        inputStream = urlAddress.openStream()
+        image = BitmapFactory.decodeStream(inputStream)
+
+        inputStream.close()
+
+        return image
+    }
+
+    private fun onPostExecuteStyle(result: Bitmap?) {
+        if (result != null) {
+            setImage(result)
+            Log.i("AsyncTask", "INSIDE IF - Showing bitmap. Thread: " +
+                    Thread.currentThread().name)
+        } else {
+            Log.i("AsyncTask", "INSIDE ELSE - Error downloading image. Thread: " +
+                    Thread.currentThread().name)
+        }
+
+        Log.i("AsyncTask", "OUT OF IF - Ending PostExecute. Thread: " +
+                Thread.currentThread().name)
+    }
+
+    private fun callAsyncTask(url: String) {
+        val download: DownloadTask = DownloadTask()
+
+        Log.i("AsyncTask", "CALLING TASK - Async being called. Thread: " +
+            Thread.currentThread().name)
+
+        download.execute(url)
     }
 
     private inner class DownloadTask() : AsyncTask<String, Int, Bitmap>() {
