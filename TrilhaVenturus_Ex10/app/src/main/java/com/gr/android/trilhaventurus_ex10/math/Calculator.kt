2964bd20d9cab5c1d918b9771edd68dcd1a292e0
giuroperto <giu.roperto@gmail.com>
2021-09-18 23:06:11 -0300

    adding tests to a calculator

--- TrilhaVenturus_Ex10/app/src/main/java/com/gr/android/trilhaventurus_ex10/math/Calculator.kt
+++ TrilhaVenturus_Ex10/app/src/main/java/com/gr/android/trilhaventurus_ex10/math/Calculator.kt
@@ -1,7 +1,47 @@
 package com.gr.android.trilhaventurus_ex10.math
 
+import kotlin.math.pow
+
 internal class Calculator {
     fun sum(first: Int, second: Int): Int {
-        return 0
+        return first + second
+    }
+
+    fun subtraction(first: Int, second: Int): Int {
+        return first - second
+    }
+
+    fun division(first: Int, second: Int): Double {
+        if (second != 0) {
+            return (first / second).toDouble()
+        }
+        return 0.0
+    }
+
+    fun multiplication(first: Int, second: Int): Int {
+        return first * second
+    }
+
+    fun power(first: Double, second: Double): Double {
+        return first.pow(second)
+    }
+
+    fun isPair(number: Int): Boolean {
+        return (number % 2 == 0)
+    }
+
+    fun isPrime(number: Int): Boolean {
+
+        var count = 2
+        var i = number - 1
+
+        if (number == 0) return false
+
+        while (i > 1) {
+            if (number % i == 0) count++
+            i--
+        }
+
+        return count == 2
     }
 }
--- TrilhaVenturus_Ex10/app/src/test/java/com/gr/android/trilhaventurus_ex10/math/CalculatorTest.kt
+++ TrilhaVenturus_Ex10/app/src/test/java/com/gr/android/trilhaventurus_ex10/math/CalculatorTest.kt
@@ -13,4 +13,186 @@ internal class CalculatorTest {
         val result = calculator.sum(2, 2)
         Assert.assertEquals(expected, result)
     }
+
+    @Test
+    fun `test when sum -2 + -2 then returns -4`() {
+        val expected = -4
+        val result = calculator.sum(-2, -2)
+        Assert.assertEquals(expected, result)
+    }
+
+    @Test
+    fun `test when subtraction -2 - -2 then returns 0`() {
+        val expected = 0
+        val result = calculator.subtraction(-2, -2)
+        Assert.assertEquals(expected, result)
+    }
+
+    @Test
+    fun `test when subtraction 2 - 2 then returns 0`() {
+        val expected = 0
+        val result = calculator.subtraction(2, 2)
+        Assert.assertEquals(expected, result)
+    }
+
+    @Test
+    fun `test when subtraction 0 - 2 then returns -2`() {
+        val expected = -2
+        val result = calculator.subtraction(0, 2)
+        Assert.assertEquals(expected, result)
+    }
+
+    @Test
+    fun `test when multiplication 0 x 2 then returns 0`() {
+        val expected = 0
+        val result = calculator.multiplication(0, 2)
+        Assert.assertEquals(expected, result)
+    }
+
+    @Test
+    fun `test when multiplication -1 x 2 then returns -2`() {
+        val expected = -2
+        val result = calculator.multiplication(-1, 2)
+        Assert.assertEquals(expected, result)
+    }
+
+    @Test
+    fun `test when multiplication -1 x -2 then returns 2`() {
+        val expected = 2
+        val result = calculator.multiplication(-1, -2)
+        Assert.assertEquals(expected, result)
+    }
+
+    @Test
+    fun `test when division 2 by 2 then returns 1`() {
+        val expected = 1.0
+        val result = calculator.division(2, 2)
+        Assert.assertEquals(expected, result, 0.5)
+    }
+
+    @Test
+    fun `test when division 2 by 0 returns error`() {
+        val expected = 0.0
+        val result = calculator.division(2, 0)
+        Assert.assertEquals(expected, result, 0.5)
+    }
+
+    @Test
+    fun `test when division 10 by -1 returns -10`() {
+        val expected = -10.0
+        val result = calculator.division(10, -1)
+        Assert.assertEquals(expected, result, 0.5)
+    }
+
+    @Test
+    fun `test when division -1 by -1 returns 1`() {
+        val expected = 1.0
+        val result = calculator.division(-1, -1)
+        Assert.assertEquals(expected, result, 0.5)
+    }
+
+    @Test
+    fun `test if 2 is pair`() {
+        val expected = true
+        val result = calculator.isPair(2)
+        Assert.assertEquals(expected, result)
+    }
+
+    @Test
+    fun `test if -2 is pair`() {
+        val expected = true
+        val result = calculator.isPair(-2)
+        Assert.assertEquals(expected, result)
+    }
+
+    @Test
+    fun `test if 0 is pair`() {
+        val expected = true
+        val result = calculator.isPair(0)
+        Assert.assertEquals(expected, result)
+    }
+
+    @Test
+    fun `test if 10 is pair`() {
+        val expected = true
+        val result = calculator.isPair(10)
+        Assert.assertEquals(expected, result)
+    }
+
+    @Test
+    fun `test if 3 is pair`() {
+        val expected = false
+        val result = calculator.isPair(3)
+        Assert.assertEquals(expected, result)
+    }
+
+    @Test
+    fun `test 10 power 1`() {
+        val expected = 10.0
+        val result = calculator.power(10.0, 1.0)
+        Assert.assertEquals(expected, result, 0.01)
+    }
+
+    @Test
+    fun `test 10 power 2`() {
+        val expected = 100.0
+        val result = calculator.power(10.0, 2.0)
+        Assert.assertEquals(expected, result, 0.01)
+    }
+
+    @Test
+    fun `test 10 power -1`() {
+        val expected = 0.1
+        val result = calculator.power(10.0, -1.0)
+        Assert.assertEquals(expected, result, 0.01)
+    }
+
+    @Test
+    fun `test 10 power 0`() {
+        val expected = 1.0
+        val result = calculator.power(10.0, 0.0)
+        Assert.assertEquals(expected, result, 0.01)
+    }
+
+    @Test
+    fun `is 10 prime`() {
+        val expected = false
+        val result = calculator.isPrime(10)
+        Assert.assertEquals(expected, result)
+    }
+
+    @Test
+    fun `is 11 prime`() {
+        val expected = true
+        val result = calculator.isPrime(11)
+        Assert.assertEquals(expected, result)
+    }
+
+    @Test
+    fun `is 17 prime`() {
+        val expected = true
+        val result = calculator.isPrime(17)
+        Assert.assertEquals(expected, result)
+    }
+
+    @Test
+    fun `is 1 prime`() {
+        val expected = true
+        val result = calculator.isPrime(1)
+        Assert.assertEquals(expected, result)
+    }
+
+    @Test
+    fun `is -1 prime`() {
+        val expected = true
+        val result = calculator.isPrime(-1)
+        Assert.assertEquals(expected, result)
+    }
+
+    @Test
+    fun `is 0 prime`() {
+        val expected = false
+        val result = calculator.isPrime(0)
+        Assert.assertEquals(expected, result)
+    }
 }
