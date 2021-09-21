7c90b067daf14cacf18534428eea40b616689763
giuroperto <giu.roperto@gmail.com>
2021-09-20 21:43:37 -0300

    adjusting to data binding

--- Ex_VM_LD/app/src/main/java/com/gr/android/ex_vm_ld/MainActivity.kt
+++ Ex_VM_LD/app/src/main/java/com/gr/android/ex_vm_ld/MainActivity.kt
@@ -4,28 +4,34 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.EditText
 import android.widget.TextView
+import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
+import com.gr.android.ex_vm_ld.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+//        setContentView(R.layout.activity_main)
 
         val viewModel = ViewModelProvider(this).get(MainActivityViewModel::class.java)
 
+        val binding: ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.lifecycleOwner = this
+        binding.viewModel = viewModel
+
         val nameTxt: TextView = findViewById(R.id.text_name)
         val nameEdit: EditText = findViewById(R.id.edt_name)
-        val otherInfoTxt: TextView = findViewById(R.id.text_other_info)
+//        val otherInfoTxt: TextView = findViewById(R.id.text_other_info)
         val otherInfoEdt: TextView = findViewById(R.id.edt_other_info)
         val saveBtn: TextView = findViewById(R.id.btn_save)
 
-        viewModel.name.observe(this, {
-            name -> nameTxt.text = name
-        })
-
-        viewModel.otherInfo.observe(this, {
-                info -> otherInfoTxt.text = info
-        })
+//        viewModel.name.observe(this, {
+//            name -> nameTxt.text = name
+//        })
+//
+//        viewModel.otherInfo.observe(this, {
+//                info -> otherInfoTxt.text = info
+//        })
 
         saveBtn.setOnClickListener {
             val inputName = nameEdit.text.toString()
@@ -35,8 +41,8 @@ class MainActivity : AppCompatActivity() {
             viewModel.updateName(inputName)
         }
 
-        nameTxt.setOnClickListener {
-            viewModel.updateAgainOtherInfo()
-        }
+//        nameTxt.setOnClickListener {
+//            viewModel.updateAgainOtherInfo()
+//        }
     }
 }
